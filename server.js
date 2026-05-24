ab4862fb33fd341d0301a0ff4632869079be8a98
cmjf88 <cmjf88@gmail.com>
2026-05-24 09:27:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -95,5 +95,31 @@ app.get('/full', async function(req, res) {
   } catch(e) { res.json({ error: true, message: e.message }); }
 });
 
+// ALL endpoint - pulls everything for all EB accounts in one shot
+app.get('/all', async function(req, res) {
+  try {
+    var session = await getSession();
+    var start = '2026-01-01';
+    var end = new Date().toISOString().split('T')[0];
+    var accountsRes = await fetch('https://www.myfxbook.com/api/get-my-accounts.json?session=' + session);
+    var accountsData = await accountsRes.json();
+    var ebAccounts = accountsData.accounts.filter(function(a) { return a.name && a.name.includes('E.B.'); });
+    var fullData = await Promise.all(ebAccounts.map(async function(account) {
+      var results = await Promise.all([
+        fetch('https://www.myfxbook.com/api/get-open-trades.json?session=' + session + '&id=' + account.id).then(function(r) { return r.json(); }),
+        fetch('https://www.myfxbook.com/api/get-history.json?session=' + session + '&id=' + account.id).then(function(r) { return r.json(); }),
+        fetch('https://www.myfxbook.com/api/get-data-daily.json?session=' + session + '&id=' + account.id + '&start=' + start + '&end=' + end).then(function(r) { return r.json(); })
+      ]);
+      return {
+        account: account,
+        openTrades: results[0],
+        history: results[1],
+        daily: results[2]
+      };
+    }));
+    res.json({ error: false, data: fullData });
+  } catch(e) { res.json({ error: true, message: e.message }); }
+});
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() { console.log('Proxy running on port ' + PORT); });
